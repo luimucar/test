bc3738f9bf027e4fd2926aa0216e567e246e89b2
Luis <Luis@Luis-PC>
2016-12-17 19:26:56 -0400

    fiveware-test-web module created: from parent clean install, then from module tomcat7:run

---
+++ fiveware-test-web/src/main/java/com/luis/config/AppConfig.java
@@ -0,0 +1,13 @@
+package com.luis.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("com.luis")
+public class AppConfig extends WebMvcConfigurerAdapter {
+
+}
